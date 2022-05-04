5e4430bf8cf4bbbef62d7506da2c73f830acd02f
Rodrigo Silva <rodsilvavieira@gmail.com>
2022-05-04 16:51:10 -0400

    chore: initial app config

---
+++ src/pages/_document.tsx
@@ -0,0 +1,36 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang='en'>
+      <Head >
+        <title>NinjaMail</title>
+        {
+          /*
+          * Raleway fonts
+          */
+        }
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+
+
+        {
+          /*
+          * Playfair fonts
+          */
+        }
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
+
+      </Head>
+
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
